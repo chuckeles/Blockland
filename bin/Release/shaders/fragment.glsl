3ca8196660c9b9b003a971e7b3a0ef781d56d025
Martin Chuckeles <martin@jakubik.info>
2015-02-04 19:37:23 +0100

    Release shaders

--- bin/Release/shaders/fragment.glsl
+++ bin/Release/shaders/fragment.glsl
@@ -1,7 +1,13 @@
 ﻿#version 440
 
+in vec3 Normal;
+
 out vec4 outColor;
 
 void main() {
-  outColor = vec4(0.2, 0.5, 1.0, 1.0);
+  vec3 color = vec3(0.7, 0.7, 0.7);
+
+  float light = 0.75 + Normal.x / 8 + Normal.y / 4 + Normal.z / 10;
+
+  outColor = vec4(color * light, 1.0);
 }
