8f95dab928da76dec57a8a123446716e3b7e92d0
Martin Chuckeles <martin@jakubik.info>
2015-02-04 23:23:16 +0100

    Debug shaders

--- bin/Debug/shaders/vertex.glsl
+++ bin/Debug/shaders/vertex.glsl
@@ -3,13 +3,14 @@
 in vec3 inPosition;
 in vec3 inNormal;
 
+uniform mat4 Model;
 uniform mat4 View;
 uniform mat4 Projection;
 
 out vec3 Normal;
 
 void main() {
-  gl_Position = Projection * View * vec4(inPosition, 1.0);
+  gl_Position = Projection * View * Model * vec4(inPosition, 1.0);
 
   Normal = inNormal;
 }
