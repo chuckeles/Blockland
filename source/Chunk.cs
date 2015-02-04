c52413f586231d0579114d5f17f5c15fe5a2711d
Martin Chuckeles <martin@jakubik.info>
2015-02-04 16:40:56 +0100

    Added Chunk, not doing anything yet

---
+++ source/Chunk.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Blockland {
+
+  public struct Block {
+
+    public struct Position {
+      public int X;
+      public int Y;
+      public int Z;
+
+      public Position(int x, int y, int z) {
+        X = x;
+        Y = y;
+        Z = z;
+      }
+    }
+
+  }
+
+  public class Chunk
+    : Component {
+
+    public Chunk()
+      : base("Chunk") {
+      mBlocks = new Dictionary<Block.Position, Block>();
+
+      mBlocks.Add(new Block.Position(0, 0, 0), new Block());
+    }
+
+    private Dictionary<Block.Position, Block> mBlocks;
+
+  }
+
+}
--- source/Program.cs
+++ source/Program.cs
@@ -26,92 +26,23 @@ namespace Blockland {
       Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView((float)Math.PI / 4, (float)window.Width / window.Height, .1f, 100f);
       shader.Uniform("Projection", ref projection);
 
-      // block
-      ArrayObject arrayObject = new ArrayObject();
-      arrayObject.Bind();
-
-      BufferObject vertex = new BufferObject(BufferObject.Type.Vertex);
-      float[] vertexData = {
-                             // front
-                             -1f, -1f,  1f,  0f,  0f,  1f,
-                              1f, -1f,  1f,  0f,  0f,  1f,
-                              1f,  1f,  1f,  0f,  0f,  1f,
-                             -1f,  1f,  1f,  0f,  0f,  1f,
-
-                             // back
-                              1f, -1f, -1f,  0f,  0f, -1f,
-                             -1f, -1f, -1f,  0f,  0f, -1f,
-                             -1f,  1f, -1f,  0f,  0f, -1f,
-                              1f,  1f, -1f,  0f,  0f, -1f,
-
-                              // right
-                              1f, -1f,  1f,  1f,  0f,  0f,
-                              1f, -1f, -1f,  1f,  0f,  0f,
-                              1f,  1f, -1f,  1f,  0f,  0f,
-                              1f,  1f,  1f,  1f,  0f,  0f,
-
-                              // left
-                             -1f, -1f, -1f, -1f,  0f,  0f,
-                             -1f, -1f,  1f, -1f,  0f,  0f,
-                             -1f,  1f,  1f, -1f,  0f,  0f,
-                             -1f,  1f, -1f, -1f,  0f,  0f,
-
-                             // top
-                             -1f,  1f,  1f,  0f,  1f,  0f,
-                              1f,  1f,  1f,  0f,  1f,  0f,
-                              1f,  1f, -1f,  0f,  1f,  0f,
-                             -1f,  1f, -1f,  0f,  1f,  0f,
-
-                             // bottom
-                              1f, -1f,  1f,  0f, -1f,  0f,
-                             -1f, -1f,  1f,  0f, -1f,  0f,
-                             -1f, -1f, -1f,  0f, -1f,  0f,
-                              1f, -1f, -1f,  0f, -1f,  0f
-                           };
-      vertex.CopyData(vertexData, true);
-      shader.Attribute("inPosition", 3, sizeof(float) * 6, 0);
-      shader.Attribute("inNormal", 3, sizeof(float) * 6, sizeof(float) * 3);
-
-      BufferObject element = new BufferObject(BufferObject.Type.Element);
-      uint[] elementData = {
-                             // front
-                             0, 1, 2,
-                             0, 2, 3,
-
-                             // back
-                             4, 5, 6,
-                             4, 6, 7,
-
-                             // right
-                             8, 9, 10,
-                             8, 10, 11,
-
-                             // left
-                             12, 13, 14,
-                             12, 14, 15,
-
-                             // top
-                             16, 17, 18,
-                             16, 18, 19,
-
-                             // bottom
-                             20, 21, 22,
-                             20, 22, 23
-                           };
-      element.CopyData(elementData, true);
+      // chunk
+      Chunk chunk = new Chunk();
+      GameObject chunkObject = new GameObject();
+      chunkObject.AddComponent(chunk);
 
       // camera object
       Transform cameraTransform = new Transform();
-      Camera cameraComponent = new Camera();
+      Camera camera = new Camera();
 
-      GameObject camera = new GameObject();
-      camera.AddComponent(cameraTransform);
-      camera.AddComponent(cameraComponent);
+      GameObject cameraObject = new GameObject();
+      cameraObject.AddComponent(cameraTransform);
+      cameraObject.AddComponent(camera);
 
       window.NativeWindow.MouseDown += (object sender, MouseButtonEventArgs e) => {
         if (e.Button == MouseButton.Left) {
-          cameraComponent.MouseLock = !cameraComponent.MouseLock;
-          window.MouseVisible = !cameraComponent.MouseLock;
+          camera.MouseLock = !camera.MouseLock;
+          window.MouseVisible = !camera.MouseLock;
         }
       };
 
@@ -131,10 +62,10 @@ namespace Blockland {
         window.Clear();
 
         // update camera
-        camera.Update(deltaTime);
+        cameraObject.Update(deltaTime);
 
         // center mouse
-        if (cameraComponent.MouseLock)
+        if (camera.MouseLock)
           window.CenterMouse();
 
         // view matrix
@@ -142,7 +73,7 @@ namespace Blockland {
         shader.Uniform("View", ref view);
 
         // rendering
-        GL.DrawElements(BeginMode.Triangles, 38, DrawElementsType.UnsignedInt, 0);
+        // GL.DrawElements(BeginMode.Triangles, 38, DrawElementsType.UnsignedInt, 0);
 
         window.Display();
 
