d30380edb7a0553711e2f43353ccb889cb07497f
Martin Chuckeles <martin@jakubik.info>
2015-02-05 23:05:00 +0100

    Slight gradient to caves

--- source/StateGame.cs
+++ source/StateGame.cs
@@ -14,7 +14,7 @@ namespace Blockland {
 
       mWindow.NativeWindow.KeyDown += OnEscape;
 
-      int blocks = 8;
+      int blocks = 16;
       mWorld.Create(blocks, 8);
 
       float halfSize = blocks * Chunk.Size * Block.Size / 2;
