27088ed0f7aeae689c99f84ab190a37f9c3fdca3
Martin Chuckeles <martin@jakubik.info>
2015-02-06 21:27:12 +0100

    Added events

---
+++ source/Events.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Blockland {
+
+  /// <summary>
+  /// Structure that holds global events.
+  /// </summary>
+  public struct Events {
+
+    #region Delegates
+
+    /// <summary>
+    /// Update event is fired once per frame.
+    /// </summary>
+    /// <param name="deltaTime">Time that has passed since the last frame</param>
+    public delegate void UpdateEvent(float deltaTime);
+
+    /// <summary>
+    /// Render event is fire once per frame after the update event.
+    /// Objects can draw to the window.
+    /// </summary>
+    public delegate void RenderEvent();
+
+    #endregion Delegates
+
+    #region Events
+
+    /// <summary>
+    /// Update event is fired once per frame.
+    /// </summary>
+    public event UpdateEvent OnUpdate;
+
+    /// <summary>
+    /// Render event is fire once per frame after the update event.
+    /// Objects can draw to the window.
+    /// </summary>
+    public event RenderEvent OnRender;
+
+    #endregion Events
+
+  }
+
+}
