a424f35e732cff7f09dd57c2282986e1f19cee23
Martin Chuckeles <martin@jakubik.info>
2015-02-06 21:42:34 +0100

    Added events

--- source/Events.cs
+++ source/Events.cs
@@ -10,31 +10,29 @@ namespace Blockland {
     #region Delegates
 
     /// <summary>
+    /// Render event is fire once per frame after the update event. Objects can draw to the window.
+    /// </summary>
+    public delegate void RenderEvent();
+
+    /// <summary>
     /// Update event is fired once per frame.
     /// </summary>
     /// <param name="deltaTime">Time that has passed since the last frame</param>
     public delegate void UpdateEvent(float deltaTime);
-
-    /// <summary>
-    /// Render event is fire once per frame after the update event.
-    /// Objects can draw to the window.
-    /// </summary>
-    public delegate void RenderEvent();
 
     #endregion Delegates
 
     #region Events
 
     /// <summary>
+    /// Render event is fire once per frame after the update event. Objects can draw to the window.
+    /// </summary>
+    public event RenderEvent OnRender;
+
+    /// <summary>
     /// Update event is fired once per frame.
     /// </summary>
     public event UpdateEvent OnUpdate;
-
-    /// <summary>
-    /// Render event is fire once per frame after the update event.
-    /// Objects can draw to the window.
-    /// </summary>
-    public event RenderEvent OnRender;
 
     #endregion Events
 
