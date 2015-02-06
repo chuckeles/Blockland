172b9857250d0196de6d17645bc9b1352ef8ce96
Martin Chuckeles <martin@jakubik.info>
2015-02-06 22:42:07 +0100

    Refactored shader

--- source/Program.cs
+++ source/Program.cs
@@ -17,27 +17,6 @@ namespace Blockland {
 
       Window.Create("Blockland", 1200, 800);
 
-      ShaderProgram shader = new ShaderProgram();
-      Shader vertexShader = new Shader(ShaderType.VertexShader, "shaders/vertex.glsl");
-      Shader fragmentShader = new Shader(ShaderType.FragmentShader, "shaders/fragment.glsl");
-
-      shader.Attach(vertexShader);
-      shader.Attach(fragmentShader);
-      shader.Link();
-      shader.Use();
-
-      State state = new StatePrepare(Window.Instance);
-      state.Start();
-
-      while (State.Current != null) {
-        State.Current.BeginFrame();
-        State.Current.Frame();
-        State.Current.EndFrame();
-      }
-
-      GC.KeepAlive(fragmentShader);
-      GC.KeepAlive(vertexShader);
-
     }
 
     #endregion Methods
