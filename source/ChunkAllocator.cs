31470144c8d3e88a782b7d8418a138187304f0bd
Martin Chuckeles <martin@jakubik.info>
2015-02-10 10:47:30 +0100

    The world now expands based on camera position

--- source/ChunkAllocator.cs
+++ source/ChunkAllocator.cs
@@ -33,19 +33,101 @@ namespace Blockland {
     /// Thread entry point.
     /// </summary>
     private void Start() {
+      // set bounds
+      int right = (int)((mRenderDistance / 2f) + 0.5f);
+      int left = -mRenderDistance / 2;
+      int front = (int)((mRenderDistance / 2f) + 0.5f);
+      int back = -mRenderDistance / 2;
+
+      // generate chunks
       lock (mChunksToGenerate) {
-        for (int x = -mRenderDistance / 2, xMax = (int)((mRenderDistance / 2f) + 0.5f); x < xMax; ++x)
-          for (int z = -mRenderDistance / 2, zMax = (int)((mRenderDistance / 2f) + 0.5f); z < zMax; ++z)
+        for (int x = left, xMax = right; x <= xMax; ++x)
+          for (int z = back, zMax = front; z <= zMax; ++z)
             for (int y = 0; y < mHeight; ++y) {
               Chunk chunk = new Chunk(x, y, z);
               mChunksToGenerate.Enqueue((int)(new Vector3i(chunk.Position.X, mHeight - chunk.Position.Y, chunk.Position.Z).Length * 90), chunk);
             }
+      }
+
+      // set camera position
+      Transform camera = State.Current.Camera["Transform"] as Transform;
+      mCameraPosition = new Vector2i((int)(camera.Position.X / (Chunk.Size * Block.Size)), (int)(camera.Position.Z / (Chunk.Size * Block.Size)));
+
+      while (World.Current != null) {
+        // check camera position
+        Vector2i newPosition = new Vector2i((int)(camera.Position.X / (Chunk.Size * Block.Size)), (int)(camera.Position.Z / (Chunk.Size * Block.Size)));
+
+        if (newPosition.X == mCameraPosition.X && newPosition.Y == mCameraPosition.Y) {
+          Thread.Sleep(1000);
+          continue;
+        }
+
+        // get delta movement
+        int deltaX = newPosition.X - mCameraPosition.X;
+        int deltaZ = newPosition.Y - mCameraPosition.Y;
+
+        // allocate new chunks
+        lock (mChunksToGenerate) {
+          if (deltaX > 0) {
+            // new chunks on the right
+            int x = right + 1;
+            ++right;
+
+            for (int z = back, zMax = front; z <= zMax; ++z)
+              for (int y = 0; y < mHeight; ++y) {
+                Chunk chunk = new Chunk(x, y, z);
+                mChunksToGenerate.Enqueue((int)(new Vector3i(chunk.Position.X - newPosition.X, mHeight - chunk.Position.Y, chunk.Position.Z - newPosition.Y).Length * 90), chunk);
+              }
+          }
+          else if (deltaX < 0) {
+            // new chunks on the left
+            int x = left - 1;
+            --left;
+
+            for (int z = back, zMax = front; z <= zMax; ++z)
+              for (int y = 0; y < mHeight; ++y) {
+                Chunk chunk = new Chunk(x, y, z);
+                mChunksToGenerate.Enqueue((int)(new Vector3i(chunk.Position.X - newPosition.X, mHeight - chunk.Position.Y, chunk.Position.Z - newPosition.Y).Length * 90), chunk);
+              }
+          }
+
+          if (deltaZ > 0) {
+            // new chunks on the front
+            int z = front + 1;
+            ++front;
+
+            for (int x = left, xMax = right; x <= xMax; ++x)
+              for (int y = 0; y < mHeight; ++y) {
+                Chunk chunk = new Chunk(x, y, z);
+                mChunksToGenerate.Enqueue((int)(new Vector3i(chunk.Position.X - newPosition.X, mHeight - chunk.Position.Y, chunk.Position.Z - newPosition.Y).Length * 90), chunk);
+              }
+          }
+          else if (deltaZ < 0) {
+            // new chunks on the back
+            int z = back - 1;
+            --back;
+
+            for (int x = left, xMax = right; x <= xMax; ++x)
+              for (int y = 0; y < mHeight; ++y) {
+                Chunk chunk = new Chunk(x, y, z);
+                mChunksToGenerate.Enqueue((int)(new Vector3i(chunk.Position.X - newPosition.X, mHeight - chunk.Position.Y, chunk.Position.Z - newPosition.Y).Length * 90), chunk);
+              }
+          }
+        }
+
+        // store camera position
+        mCameraPosition = newPosition;
       }
     }
 
     #endregion Methods
 
     #region Fields
+
+    /// <summary>
+    /// Stored camera position.
+    /// </summary>
+    private Vector2i mCameraPosition;
 
     /// <summary>
     /// World height in chunks.
