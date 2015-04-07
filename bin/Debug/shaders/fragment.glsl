8e45ba8ded69579ffdcc6da52f6b49e1fa3559cc
Martin Chuckeles <martin@jakubik.info>
2015-04-07 10:46:47 +0200

    Shaders have been copied to bin/debug

--- bin/Debug/shaders/fragment.glsl
+++ bin/Debug/shaders/fragment.glsl
@@ -1,21 +1,58 @@
 ﻿#version 440
 
-in vec3 Normal;
+// input
 in vec3 TexCoord;
-in float Depth;
+in vec3 EyeSun;
+in vec3 EyeSun2;
+in vec3 EyePosition;
+in vec3 EyeNormal;
+in float BlockLight;
 
-uniform sampler2DArray Texture;
+// textures
+uniform sampler2DArray uTexture;
+uniform sampler2DArray uNormalTexture;
 
+// output
 out vec4 outColor;
 
-void main() {
-  const float e = 2.71828182845904523536028747135266249;
+// ambient lighting model
+float Ambient() {
+  return 0.1;
+}
 
+// diffuse lighting model
+float Diffuse(vec3 normal) {
+  float sunDotNormal = max(dot(EyeSun, normal), 0.0);
+  float sunDotNormal2 = max(dot(EyeSun2, normal), 0.0);
+  
+  float diffuse = 0.8 * sunDotNormal + 0.2 * sunDotNormal2;
+  
+  return Ambient() + diffuse * BlockLight;
+}
+
+// phong lighting model
+float PhongModel(vec3 position, vec3 normal) {
+  vec3 v = normalize(-position);
+  vec3 h = normalize(v + EyeSun);
+  
+  float specular = pow(max(dot(v, h), 0.0), 40.0) * 0.4;
+  
+  return Diffuse(normal) + specular;
+}
+
+void main() {
+  // calculate fog
+  const float e = 2.71828182845904523536028747135266249;
   vec4 fogColor = vec4(0.7, 0.9, 1.0, 1.0);
   float fogDensity = 0.0025;
-
-  float light = 0.5 + Normal.x * 0.1 + Normal.y * 0.4 + Normal.z * 0.05;
-
-  vec4 color = texture(Texture, TexCoord) * vec4(light, light, light, 1.0);
-  outColor = mix(fogColor, color, pow(e, -pow(Depth * fogDensity, 2)));
+  float fog = pow(e, -pow(length(EyePosition) * fogDensity, 2));
+  
+  // calculate light
+  float light = Diffuse(EyeNormal);
+  
+  // get texture color
+  vec4 texColor = texture(uTexture, TexCoord);
+  
+  // calculate output
+  outColor = mix(fogColor, texColor * light, fog);
 }
--- bin/Debug/shaders/vertex.glsl
+++ bin/Debug/shaders/vertex.glsl
@@ -1,22 +1,35 @@
 ﻿#version 440
 
+// input
 in vec3 inPosition;
 in vec3 inNormal;
 in vec3 inTexCoord;
+in float inLight;
 
-uniform mat4 Model;
-uniform mat4 View;
-uniform mat4 Projection;
+// matrices
+uniform mat4 uModelView;
+uniform mat4 uModelViewProjection;
+uniform mat3 uNormal;
 
-out vec3 Normal;
+// output
 out vec3 TexCoord;
-out float Depth;
+out vec3 EyeSun;
+out vec3 EyeSun2;
+out vec3 EyePosition;
+out vec3 EyeNormal;
+out float BlockLight;
 
 void main() {
-  gl_Position = Projection * View * Model * vec4(inPosition, 1.0);
-  
-  Normal = inNormal;
-  TexCoord = inTexCoord;
+  // sun directions
+  vec3 sun = normalize(vec3(0.3, 1.0, 0.6));
+  vec3 sun2 = normalize(vec3(-sun.x, sun.y, -sun.z));
 
-  Depth = length((View * Model * vec4(inPosition, 1.0)).xyz);
+  // calculate output
+  gl_Position = uModelViewProjection * vec4(inPosition, 1.0);
+  TexCoord = inTexCoord;
+  EyeSun = normalize(uNormal * sun);
+  EyeSun2 = normalize(uNormal * sun2);
+  EyePosition = (uModelView * vec4(inPosition, 1.0)).xyz;
+  EyeNormal = normalize(uNormal * inNormal);
+  BlockLight = inLight;
 }
